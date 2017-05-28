f77faaa3df9bf1d1d165f6fa3fed85990770b86c
hoodielive <hoodielive@gmail.com>
2017-05-28 09:14:35 -0500

    Added merge.rb program

---
+++ codeblocks/merge.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby 
+# This is for hashes only 
+
+h1 = {:a => 111, :b => 222 } 
+h2 = {:b => 333, :c => 444 } 
+
+puts h1.merge(h2) 
+puts h2.merge(h1) 
+
+h1.merge(h2) {|key,old,new| new } 
+# or 
+
+
+h1.merge(h2) {|key,old,new| old * 2 }
+
+
+h1.merge(h2) do |key, old, new|
+	if old < new 
+		old
+	else
+		new
+	end
+end
+
