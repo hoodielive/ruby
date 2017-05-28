d0b41d60d572367eeefcac09a4af3673b8b6b4dc
hoodielive <hoodielive@gmail.com>
2017-05-28 06:31:04 -0500

    Control structures

---
+++ control-structures.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby 
+#
+# Control Structures provide actions 
+
+
+#if boolean
+#	... 
+#end
+
+
+#if  boolean 
+#	puts "something"
+#elsif x > 20
+#	puts "something more"
+#else 
+#	puts "10 < 20"
+#end
+
+name = "lima"
+
+if name == "bob" 
+	puts "found him"
+else 	
+	puts "not him" 
+
+end 
+
+puts "This is lima" if name == "lima" 
