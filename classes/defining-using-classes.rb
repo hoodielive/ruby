2ce17bc6e9f3c4dd5acf3dc1c51c027ed10ceeea
hoodielive <hoodielive@gmail.com>
2017-05-28 15:01:35 -0500

    Defining and Using Classes

---
+++ classes/defining-using-classes.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+
+#class SomeName (needs to begin with Capital letter or any proceeding should be camelcase 
+#	... 
+#end
+
+class Animal
+
+	def make_noise
+		"Moo!" 
+	end
+
+end
+
+#Animal.new 
+
+animal = Animal.new 
+
+animal.make_noise
