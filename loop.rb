951130ce80ac95ec8afd0eb8185709ecf29788c2
hoodielive <hoodielive@gmail.com>
2017-05-28 06:52:28 -0500

    Loops

---
+++ loop.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby 
+#
+#loop do 
+#	...
+# end 
+#
+
+# break - terminate the whole loop
+# next = jump to the next loop
+# redo = redo this loop 
+# retry = start the whole loop over from the beginning 
+# while = while a boolean is true 
+# until = works likes less, unless - means while something is untrue 
+
+x = 0 
+
+loop do 
+	x += 2 # increment by 2  
+	break if x >=20 # break when x equals 20 
+	puts x # puts the value of x to output but will not have 20, will stop at 18
+end
+
+
+loop do 
+	x += 2
+	break if x >= 20 
+	next if x == 6 
+	puts x 
+end 
+
+
+
+
