bde228e1911d452b7aee9a29a7ad2aa91d7ea10e
hoodielive <hoodielive@gmail.com>
2017-05-28 15:23:53 -0500

    Added reader/writer methods (setters and getters

---
+++ classes/reader-writer-methods.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# Getters and Setters
+
+class Animal
+    def noise=(noise)
+        @noise = noise
+    end
+
+    def noise
+        @noise
+    end
+end
+
+animal1 = Animal.new
+animal1.noise = "Moo!"
+puts animal1.noise
+
+animal2 = Animal.new
+animal2.noise = "Quack!"
+puts animal2.noise
