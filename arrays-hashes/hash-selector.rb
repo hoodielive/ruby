57f6e69acab9ed40888185ddd1e215f7eff4f535
hoodielive <hoodielive@gmail.com>
2017-05-29 15:52:55 -0400

    Hash Selector :: Needs work

---
+++ arrays-hashes/hash-selector.rb
@@ -0,0 +1,21 @@
+class HashSelector 
+
+	def initalize(hash)
+	  @hash = hash 
+	end 
+
+	def meaningful_keys 
+	  hash.select! { |k,v| k == :valid }
+	end 
+
+	private 
+	
+	attr_reader :hash 
+
+end 
+
+a == HashSelector.new({:invalid => :value, :valid => :another_value}) 
+
+p a.meaningful_keys 
+
+
