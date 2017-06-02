2b7d1c830aef301c0db195b6697f8432e9ae28bf
hoodielive <hoodielive@gmail.com>
2017-06-01 19:15:26 -0500

    Make a person with commentary :: solidify concepts

---
+++ classes/the_making_of_a_person.rb
@@ -0,0 +1,9 @@
+class Person
+    attr_accessor :name, :age, :gender
+end
+
+# a class is a concept, a blueprint, a definition of an object - should always start with a capital letter
+# proceeding line creates 'attributes' (attr) and accessor means 'make these attr accessible to be set or changed at will'
+# thus, Person object and we can change the name, age and gender or more accurately name,age,gender attributes
+
+    
