5ee9669982ff825a18815f8513b5522ac7695ae0
hoodielive <hoodielive@gmail.com>
2017-06-01 19:23:28 -0500

    Instantiated Class

--- classes/the_making_of_a_person.rb
+++ classes/the_making_of_a_person.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 class Person
     attr_accessor :name, :age, :gender
 end
@@ -6,4 +7,12 @@ end
 # proceeding line creates 'attributes' (attr) and accessor means 'make these attr accessible to be set or changed at will'
 # thus, Person object and we can change the name, age and gender or more accurately name,age,gender attributes
 
-    
+
+# The Template is there - now the energy
+
+person_instance = Person.new
+person_instance.name = 'Someone'
+person_instance.age = '33'
+person_instance.gender = 'male'
+
+puts person_instance.name person_instance.age person_instance.gender
