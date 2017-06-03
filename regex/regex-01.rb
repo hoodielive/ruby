bd715bd7e4af96b8da724e5e3aca991d8e75f38f
hoodielive <hoodielive@gmail.com>
2017-06-02 20:50:30 -0500

    Ruby gets better and better

---
+++ regex/regex-01.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby 
+#
+
+puts "This is a test string".scan(/[aeiou]/)
+
+
+puts "This ia test" if "this is also a test" =~ /[aeiou]/ 
+
+
+puts "Ruby is dope".match(/\w+) (/\w+) 
+
+
