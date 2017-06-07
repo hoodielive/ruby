c279d8154716c12a4a3aa27efe69b573e8b098a7
hoodielive <hoodielive@gmail.com>
2017-06-06 22:41:08 -0400

    App01 Analyzer

---
+++ apps/analyzer.rb
@@ -0,0 +1,18 @@
+line_count = 0 
+File.open("text.txt").each {|line| line_count += 1 }
+puts line_count 
+
+text='' 
+line_count = 0 
+File.open("text.txt").each do |line| 
+	line_count += 1 
+	text << line
+end
+
+puts "#{line_count} lines" 
+
+lines = File.readlines("text.txt") 
+line_count = lines.size
+text = lines.join 
+
+puts "#{line_count} lines" 
