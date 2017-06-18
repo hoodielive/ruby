afbb5eeed6f7536f31021b0592694cf71da5fca1
hoodielive <hoodielive@gmail.com>
2017-06-17 22:55:40 -0400

    Beefing up the Analyzer

--- apps/analyzer.rb
+++ apps/analyzer.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby 
+# character count (cc), cc excluding spaces, line count, word count, sentence count, paragraph count, avg num of wps w/paragraph 
+
 line_count = 0 
 File.open("text.txt").each {|line| line_count += 1 }
 puts line_count 
