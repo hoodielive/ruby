e087771afc1162d84397d03fff845fd65dbd41de
hoodielive <hoodielive@gmail.com>
2018-05-09 23:48:53 -0400

    R_and_Ruby simple comp

--- R_and_Ruby/simple_comp.rb
+++ R_and_Ruby/simple_comp.rb
@@ -4,3 +4,15 @@ a.each do |i|
   puts " #{i.length} is #{i}"
 end
 
+b = {
+	name: 'Vincent', 
+	age: 32, 
+	location: 'USA',
+	profession: 'Python Developer'
+} 
+
+
+b.each { |k,v| puts "#{k} #{v}" } 
+
+
+
