7473f0d1c8f4df6c66500f089ae0175a3bb6d142
hoodielive <hoodielive@gmail.com>
2018-08-10 22:37:30 -0400

    expanding this bs

--- puppet/square_class.rb
+++ puppet/square_class.rb
@@ -7,10 +7,24 @@ class Square
     def side_length
         return @side_length
     end
-end
 
+    def side_length=(side_length)
+        return @side_length = side_length
+    end
+
+    def perimeter 
+        return @side_length * 4 
+    end
+
+    def perimeter 
+        return @side_length * 4 
+    end
+end
 
 new_method = Square.new(10) 
 
 puts new_method.side_length() 
+
+puts new_method.perimeter()
+
 
