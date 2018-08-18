a4b1dd68b0d7a32be436da6e4906c53d951fc3c8
hoodielive <hoodielive@gmail.com>
2018-08-18 11:39:46 -0400

    Marshal

---
+++ ruby_core/File/file.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+# File Class 
+#
+
+File.open("example.txt", "a+") do |file|
+	
+	print "Enter your name: " 
+	
+	name = gets.chomp
+	
+	file.puts "Name: #{name}"
+
+	print "Enter your email: " 
+	
+	email = gets.chomp
+	
+	file.puts "Email: #{email}"
+
+end
