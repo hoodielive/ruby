ab6d7f01b47b4c1ff3bf46f8c72f141dd1d66328
hoodielive <hoodielive@gmail.com>
2018-08-18 10:56:14 -0400

    Object Kernel

---
+++ ruby_core/Object_Kernel/Array_kernel.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby 
+# 
+# Kernel Arrays
+# 
+string = ""
+puts String.superclass
+puts Array(1) 
+puts String.new
+puts String.methods.sort 
+puts String.respond_to?(:strip)
+puts string.respond_to?(:strip)
+puts string.respond_to?(:stripes)
