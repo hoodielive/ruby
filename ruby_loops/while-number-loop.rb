4d8f5a78eae1a6a78ec11d844d140a05e2ab17d8
hoodielive <hoodielive@gmail.com>
2018-08-20 08:00:13 -0400

    Until

---
+++ ruby_loops/while-number-loop.rb
@@ -0,0 +1,15 @@
+def print_hello(number_of_times) 
+	i = 0 
+	while i < number_of_times
+		puts "hello"
+		i += 1 
+	end
+end
+
+answer = 0
+while answer < 5
+	print "how many times do you want to print 'hello'? Enter a number greater than 5 to exit) "
+	answer = gets.chomp.to_i
+	print_hello(answer)
+end
+
