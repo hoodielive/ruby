762b8680701e023f2850b960ca096bb53d3864d3
hoodielive <hoodielive@gmail.com>
2018-11-15 21:27:27 -0500

    no comments

--- attr/attr.rb
+++ attr/attr.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 # code does not work and needs work {tired lol}
+
 class PracticeAttributAccessor
 
     attr_accessor :value, :names
-        define_method(:value) { puts "Hello" } end
+        define_method(:value) { puts "Hello" } 
 
     # defines a named attribute for this module, where the name is sysmbol.id2name, creating an instance (@name) and a corresponding access method to read it. Also creates a method called name = to set the attribute. String arguments are converted to symbols
 end
