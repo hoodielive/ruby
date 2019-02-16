0ec179f1dcb89dc9f3b38fa7ff5cee5e73cb58f6
hoodielive <hoodielive@gmail.com>
2019-02-16 11:06:20 -0500

    ..refactor thumbs_up and thumbs_down

---
+++ pragmatic/movie_class.rb
@@ -0,0 +1,27 @@
+class Movie
+  # declares Movie title and rank
+  def initialize(title, rank)
+    @title = title.capitalize
+    @rank = rank
+    puts "Created a movie object with title #{title} and rank #{rank}"
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank}"
+  end
+end
+
+movie1 = Movie.new('goonies', 10)
+movie1.thumbs_up
+puts movie1.to_s
+
+movie2 = Movie.new('ghostbusters', 8)
+puts movie2.to_s
