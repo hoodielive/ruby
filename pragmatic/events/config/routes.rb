693b4115fa89e29fdca5785cf42635fa8cb78642
hoodielive <hoodielive@gmail.com>
2019-02-19 05:46:37 -0500

    controllers and erbs

--- pragmatic/events/config/routes.rb
+++ pragmatic/events/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # root route
+  root 'demo#index'
+
+  # simple route
   get 'demo/index'
+  get 'demo/hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "events" => "events#index"
+  # get "events" => "events#index"
+  # default route may go away in future versions of rails :(
+  # get ':controller(/:action(/:id))/:id'
 end
