bda44157db28b93be5eec2462b11686db3d3fac1
hoodielive <hoodielive@gmail.com>
2019-02-19 20:50:57 -0500

    added some erb logic

--- pragmatic/events/app/controllers/demo_controller.rb
+++ pragmatic/events/app/controllers/demo_controller.rb
@@ -2,14 +2,18 @@ class DemoController < ApplicationController
   layout false
 
   def index
-    render('hello')
+    render('index')
   end
 
   def hello
-    render('index')
+    render('hello')
   end
 
   def other_hello
     redirect_to(:action => 'index')
+  end
+
+  def youtube
+    redirect_to('http://youtube.com')
   end
 end
--- pragmatic/events/config/routes.rb
+++ pragmatic/events/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
+  get 'demo/youtube'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "events" => "events#index"
