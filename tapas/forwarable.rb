be235dcc71224c5f9662a7539ffa57b3cce23cd4
hoodielive <hoodielive@gmail.com>
2019-02-21 05:48:00 -0500

    ..work with forwardable

---
+++ tapas/forwarable.rb
@@ -0,0 +1,28 @@
+class User
+  attr_reader :account
+  def initialize(account)
+    @account = account
+  end
+
+  def first_name
+    account.first_name
+  end
+
+  def last_name
+    account.last_name
+  end
+
+  def full_name
+    "#{firt_name} #{last_name}"
+  end
+
+  def email_address
+    account.email_address
+  end
+end
+
+GithubAccount = Struct.new(:uid, :email_address, :first_name, :last_name)
+FaceBookAccount = Struct.new(:uid, :email_address, :first_name, :last_name)
+
+hoodie = User.new(GithubAccount.new('hoodie', 'hoodie@somewhere.org', 'Hoodie', 'Grimm'))
+hoodie.full_name
