07c0dd784f879e198fda51b5771e9d6a798730e2
hoodielive <hoodielive@gmail.com>
2019-04-29 22:23:34 -0400

    building a string from parts

--- cook/building_a_string_from_parts.rb
+++ cook/building_a_string_from_parts.rb
@@ -2,12 +2,17 @@ hash = { key1: "val1", key2: "val2" }
 string = ""
 hash.each { |k,v| string << "#{k} is #{v}\n"}
 puts string 
-
 data = ['1', '2', '3']
-
 s = ''
 data.each { |x| s << x << ' and a '}
 puts s 
 data.join(' and a ')
 puts s
+s = ""
+data.each_with_index { |x, i| s << x; s << "|" if i < data.length - 1 }
+puts s 
+
+number = 5
+
+puts "#{number}" == '5'
 
